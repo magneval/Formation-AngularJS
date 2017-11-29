06d66b0f402fd7202b818bb5a72aa7cddf9bea0d
Charles de MAGNEVAL <magneval@gmail.com>
2017-11-29 19:53:44 +0100

    Mise en place du deuxième Exo changement de la gestion du scope a la mode Angular

--- TPs/TP2/js/CommandeControlleur.js
+++ TPs/TP2/js/CommandeControlleur.js
@@ -1,12 +1,12 @@
 angular.module('exo', [])
 .controller('CommandeControlleur', 
-    function() {
-        this.liste = [
+    function($scope) {
+        $scope.liste = [
             {id: 1, libelle: "test", montant: 10},
             {id: 2, libelle: "demo", montant: 100}
         ];
-        this.add = function () {
-            this.liste.push(
+        $scope.add = function () {
+            $scope.liste.push(
                     {id: 3, libelle: "ajout", montant: 500}
             );
         };
