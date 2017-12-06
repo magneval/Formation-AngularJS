488dd8e172d18dfe9cca50dc882db036c8cb22a5
Charles de MAGNEVAL <magneval@gmail.com>
2017-12-06 12:38:01 +0100

    Mise en place du quatrième Exo

--- TPs/TP3/js/Application.js
+++ TPs/TP3/js/Application.js
@@ -1,5 +1,12 @@
 angular.module('commandeApp',['ngRoute'])
 .config(function($routeProvider, $locationProvider){
+  //V1.2 avec url contennant le # et le ! pour être compatible AngularJS 1.6
+        $routeProvider.when('/!Commandes/:id',{
+                templateUrl: 'views/detail.html',
+                controller: 'DetailControlleur'
+            }
+        );
+  //V1.6
         $routeProvider.when('/Commandes/:id',{
                 templateUrl: 'views/detail.html',
                 controller: 'DetailControlleur'
